ab2d61183040d10584276ae411a86e56573aab7a
Matt Nieland <matt@copperline.io>
2026-05-29 20:29:26 -0500

    feat: add DEX_ADMIN_PASSWORD_HASH export in entrypoint script

--- dex/entrypoint.sh
+++ dex/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
 mkdir -p /var/dex && touch /var/dex/dex.db
+export DEX_ADMIN_PASSWORD_HASH=$(/usr/local/bin/dex bcrypt-hash "$DEX_ADMIN_PASSWORD")
 envsubst < /etc/dex/config.template.yaml > /etc/dex/config.yaml
 exec /usr/local/bin/dex serve /etc/dex/config.yaml
